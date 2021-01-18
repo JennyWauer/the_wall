d5d76c694991385bace208cb559c8313453c39e2
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-18 08:21:47 -0600

    debug initial set up

--- codingdojo_wall/urls.py
+++ codingdojo_wall/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('/codingdojo_wall', views.wall),
+    path('', views.wall),
 ]
--- the_wall/urls.py
+++ the_wall/urls.py
@@ -17,5 +17,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('login.urls')),
-    path('/codingDojo_wall', include('codingdojo_wall.urls')),
+    path('codingdojo_wall/', include('codingdojo_wall.urls')),
 ]
