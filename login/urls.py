541ee72f2c19b85037a88a054134390bcbe6800d
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-18 13:35:07 -0600

    add login views and urls

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.home),
+    path('success', views.success),
+    path('register', views.register),
+    path('user_login', views.user_login),
 ]
--- login/views.py
+++ login/views.py
@@ -1,4 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-def index(request):
-    return render(request, 'index.html')
+from .models import *
+
+from django.contrib import messages
+
+import bcrypt
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def success(request):
+    if 'userid' in request.session:
+        user = User.objects.filter(id=request.session['userid'])
+        if user:
+            context = {
+                "user": user[0]
+            }
+        return render(request, 'success.html', context)
+    return redirect('/')
+
+def register(request):
+    if request.method == 'POST':
+        errors = User.objects.basic_validator(request.POST)
+        if User.objects.filter(email=request.POST['email']):
+            messages.error(request, 'Email is already registered. Please login!')
+            return redirect('/')
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        else:
+            password = request.POST['password']
+            pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()    
+            new_user = User.objects.create(
+                first_name=request.POST['first_name'],
+                last_name=request.POST['last_name'],
+                email=request.POST['email'],
+                password=pw_hash
+            )
+            request.session['userid'] = new_user.id
+            messages.success(request, "User successfully created")
+            return redirect('/success')
+    return redirect('/')
+
+def user_login(request):
+    if request.method == 'GET':
+        return redirect('/')
+    if request.method == 'POST':
+        user = User.objects.filter(email=request.POST['login_email'])
+        if user:
+            logged_user = user[0] 
+            if bcrypt.checkpw(request.POST['login_pass'].encode(), logged_user.password.encode()):
+                request.session['userid'] = logged_user.id
+                return redirect('/success')
+        else:
+            messages.error(request, 'Email/password combination not recognized. Please try again!')
+        return redirect("/")
