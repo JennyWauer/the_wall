d75c8302a583cdb32cbd625d815aa7c1034360d6
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-18 20:23:38 -0600

    add add_message view

--- codingdojo_wall/models.py
+++ codingdojo_wall/models.py
@@ -9,7 +9,7 @@ class Message(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
-    user = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
+    user_id = models.ForeignKey(User, related_name="comments", on_delete = models.CASCADE)
     message_id = models.ForeignKey(Message, related_name="comments", on_delete = models.CASCADE)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- codingdojo_wall/views.py
+++ codingdojo_wall/views.py
@@ -9,3 +9,10 @@ def wall(request):
         "user": User.objects.filter(id=request.session['userid']),
     }
     return render(request, 'wall.html', context)
+
+def add_message(request):
+    if request.method == 'GET':
+        return redirect('/wall')
+    if request.method == 'POST':
+        Message.objects.create(user_id=request.POST['user_id'],message=request.POST['message'])
+        return redirect('/wall')
