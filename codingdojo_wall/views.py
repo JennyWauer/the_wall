c77bea12622adda451fb42adc3f350baaa97bbb5
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-19 13:52:56 -0600

    debug add message

--- codingdojo_wall/views.py
+++ codingdojo_wall/views.py
@@ -6,7 +6,9 @@ from login.models import User
 
 def wall(request):
     context = {
-        "user": User.objects.filter(id=request.session['userid']),
+        "user": User.objects.get(id=request.session['userid']),
+        "messages": Message.objects.all(),
+        "comments": Comment.objects.all(),
     }
     return render(request, 'wall.html', context)
 
@@ -14,12 +16,12 @@ def add_message(request):
     if request.method == 'GET':
         return redirect('/wall')
     if request.method == 'POST':
-        Message.objects.create(user_id=request.POST['user_id'],message=request.POST['message'])
+        Message.objects.create(user_id=User.objects.get(id=request.session['userid']),message=request.POST['message'])
         return redirect('/wall')
 
 def add_comment(request):
     if request.method == 'GET':
         return redirect('/wall')
     if request.method == 'POST':
-        Comment.objects.create(user_id=request.POST['user_id'],message_id=request.POST['message_id'],comment=request.POST['comment'])
+        Comment.objects.create(user_id=User.objects.get(id=request.session['userid']),message_id=Message.objects.get(id=request.POST['message_id']),comment=request.POST['comment'])
         return redirect('/wall')
