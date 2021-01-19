1627a09edb26360ed7c9def9d41ff777e232c4c1
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-18 20:14:20 -0600

    add imports for views

--- codingdojo_wall/views.py
+++ codingdojo_wall/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import *
+
+from login.models import User
 
 def wall(request):
     return render(request, 'wall.html')
